ee38aa5742c603edbc6cf89f2586828e8395c5b8
nimisha-gj <nimisha@infraspec.dev>
2024-07-17 15:41:24 +0530

    update backend config

--- main.tf
+++ main.tf
@@ -8,12 +8,3 @@ provider "aws" {
   }
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "testing-terragrunt-infraspec"    # Replace with your S3 bucket name
-    key            = "terragrunt.tfstate" # Replace with the path where the state file will be stored
-    region         = "ap-south-1"                      # Replace with your AWS region
-    dynamodb_table = "testing-terragrunt-infraspec"            # Replace with your DynamoDB table name
-    encrypt        = true
-  }
-}
